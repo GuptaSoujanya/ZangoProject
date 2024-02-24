c4e71005ab8a52ecad597e0ece921d6197e38f14
soujanya gupta <semrisoujanya2003@gmail.com>
2024-02-24 14:29:13 +0530

    add new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import  {useState, useEffect }from 'react';
+import Navbar from './components/Navbar';
+import Navbar2 from './components/Navbar2'
+
 
 function App() {
+  const [currentNavbar, setCurrentNavbar] = useState('Navbar');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Change the current navbar after 10 seconds
+      setCurrentNavbar(currentNavbar === 'Navbar' ? 'Navbar2' : 'Navbar');
+    }, 8000); // Change navbar every 10 seconds
+
+    // Clear the interval on component unmount
+    return () => clearInterval(interval);
+  }, [currentNavbar]); // Re-run effect when currentNavbar changes
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {currentNavbar === 'Navbar' ? <Navbar /> : <Navbar2 />}
     </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/components/Navbar2.js
@@ -0,0 +1,40 @@
+import {React} from 'react';
+import logo1 from '../assets/Image/Cat-N-Dog-Logo copy.png'
+import playground1 from '../assets/Image/google-play-badge.png'
+import playground2 from '../assets/Image/AppStoreBadge.png'
+
+function Navbar() {
+    return (
+        <body>
+        <div id="maincontainer">
+        <div id="leftcontainer">
+            <div id="logo">
+                <img src={logo1} alt=""/>
+            </div>
+           <div id="textcontainer">
+            <h2>Lorem Ipsum  is simply dummy text  of the printing and typesetting industry</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum voluptas, perferendis suscipit doloribus nesciunt facere provident itaque! Optio tenetur voluptat</p>
+           </div>
+           <div id="btnimages">
+            <img src={playground2} alt="playbage1"/>
+            <img src={playground1} alt="playbadge2"/>
+           </div>
+        </div>
+        <div id="rightcontainer1">
+            <div id="nav">
+                <a href="#">About</a>
+                <a href="#">Blog</a>
+                <a href="#">Carrers</a>
+                <a href="#">Contact</a>
+            </div>
+            <div id="textcontent">
+                <h3>Type Text</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut </p>
+            </div>
+        </div>
+        </div>
+    </body>
+    );
+  }
+  
+  export default Navbar;
